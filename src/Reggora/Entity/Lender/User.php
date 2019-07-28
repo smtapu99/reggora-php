b9c514399e8ac000e69b642657bcc21ba19c259b
smtapu99 <shahadatmahmud99@gmail.com>
2019-07-28 19:38:06 -0400

    Add `User` entity

---
+++ src/Reggora/Entity/Lender/User.php
@@ -0,0 +1,116 @@
+<?php
+namespace Reggora\Entity\Lender;
+
+use Psr\Http\Message\ResponseInterface;
+use Reggora\Lender;
+use Reggora\Entity\AbstractEntity;
+
+use Illuminate\Support\Collection;
+
+/**
+ * @property mixed id
+ */
+final class User extends AbstractEntity {
+
+    /**@var Collection|null*/
+    public $matched_users;
+
+    public function __construct(array $data)
+    {
+        parent::__construct($data);
+
+        if(isset($data['matched_users']) && is_array($data['matched_users']))
+        {
+            $users = [];
+            foreach($data['matched_users'] as $user)
+            {
+                $users[] = new User($user);
+            }
+
+            $this->matched_users = new Collection($users);
+        }
+    }
+
+    /**
+     * Find a user by it's string id
+     *
+     * @param string $id
+     * @return User|null
+     */
+    public static function find(string $id)
+	{
+		$json = Lender::getInstance()->getAdapter()->get(sprintf('lender/user/%s', $id));
+
+		if(!empty($json) && isset($json['id']))
+		{
+			return new User($json);
+		}
+		
+		return null;
+	}
+
+    /**
+     * Invite a user
+     *
+     * @param array $parameters
+     * @return User|null
+     */
+    public static function invite(array $parameters)
+	{
+		$user = Lender::getInstance()->getAdapter()->post('lender/users', $parameters);
+		return self::find($user);
+	}
+
+    /**
+     * Delete a user
+     *
+     * @return ResponseInterface
+     */
+    public function delete()
+	{
+		return Lender::getInstance()->getAdapter()->delete(sprintf('lender/user/%s', $this->id));
+	}
+
+    /**
+     * Save a user
+     */
+    public function save()
+	{
+		Lender::getInstance()->getAdapter()->put(sprintf('lender/user/%s', $this->id), $this->getDirtyData());
+		$this->clean();
+	}
+
+    /**
+     * Get all users
+     *
+     * @param int $offset
+     * @param int $limit
+     * @param string $ordering
+     * @param null $userOfficer
+     * @return Collection
+     */
+    public function all(string $ordering = '-created', string $search = null)
+	{
+		$users = Lender::getInstance()->getAdapter()->get('lender/users', [
+			'ordering' => $ordering,
+            'search' => $search,
+		]);
+
+		foreach($users as $key => $data)
+		{
+			$users[$key] = new self($data);
+		}
+
+		return new Collection($users);
+	}
+
+    /**
+     * Get the entity identifier
+     *
+     * @return mixed|string
+     */
+    public function getIdentifier()
+	{
+		return 'id';
+	}
+}
