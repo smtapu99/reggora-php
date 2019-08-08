637035672ca2b9b194bc12aaabec6cd7ffdbc1ac
smtapu99 <shahadatmahmud99@gmail.com>
2019-08-08 13:36:33 -0400

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\Yaml\Yaml;
 use Reggora\Lender;
+use Reggora\Vendor;
 
 $config = Yaml::parse(file_get_contents('./tests/credentials.yml'));
 
 new Lender($config['lenderEmail'], $config['lenderPassword'], $config['lenderIntegrationToken']);
+new Vendor($config['lenderEmail'], $config['lenderPassword'], $config['lenderIntegrationToken']);
